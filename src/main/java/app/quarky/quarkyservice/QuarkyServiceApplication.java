3a8bbb7b998e0685d272098b20558a03180d9b35
Henri <henri.schatz@gmx.de>
2023-04-23 19:35:55 +0200

    Initial commit

---
+++ src/main/java/app/quarky/quarkyservice/QuarkyServiceApplication.java
@@ -0,0 +1,13 @@
+package app.quarky.quarkyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuarkyServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuarkyServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/app/quarky/quarkyservice/QuarkyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package app.quarky.quarkyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuarkyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
